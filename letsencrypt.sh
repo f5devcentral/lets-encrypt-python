760349bcc49fdaec49c751a2b3f94b441cfd354e
rahm <j.rahm@f5.com>
2017-11-28 11:33:41 -0600

    Fixed paste error for letsencrypt.sh script
    Looks like original script URL not accurate any longer, might need to update to use new "dehydrated" script.

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,4 +1,6 @@
-h by lukas2511
+#!/usr/bin/env bash
+
+# letsencrypt.sh by lukas2511
 # Source: https://github.com/lukas2511/letsencrypt.sh
 #
 # This script is licensed under The MIT License (see LICENSE for more information).
